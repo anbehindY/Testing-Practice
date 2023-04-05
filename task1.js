2f345dc6f168b79570c1743ddf8c9688072ee43e
anbehindY <yanminthwin@gmail.com>
2023-04-05 16:15:11 +0630

    Add new function to task1

--- task1.js
+++ task1.js
@@ -1,5 +1,9 @@
 const stringLength = (string) => {
-    return string.length;
+    if (string.length >= 1 && string.length < 11){
+        return string.length;
+    } else {
+        throw new Error('String length out of range');
+    }
 }
 
 module.exports = stringLength;
--- task1.test.js
+++ task1.test.js
@@ -1,9 +1,10 @@
 const stringLength = require('./task1');
 
-// if (stringLength(hello) !== 5) {
-//     throw new Error('Length check failed')
-// }
-
-test ('Length of "hello" to equal 5' ,() => {
+test ('Length of "hello" to equal 5' , () => {
     expect(stringLength('hello')).toBe(5);
+});
+
+test ('Length of "hello jenna" to throw error', () => {
+    expect(() => stringLength('hello jenna'))
+    .toThrow('String length out of range');
 });
